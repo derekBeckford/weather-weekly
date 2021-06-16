aef135b9181a3bbfffd87531a83fdcaa13e240a4
Derek Beckford <dbeck1207@icloud.com>
2021-06-15 19:20:52 -0500

    logic for UV color

--- assets/logic.js
+++ assets/logic.js
@@ -8,8 +8,8 @@ $(".search-button").click(function(){
 })
 
 var currentForecast = function(){
-    var citySearched = "Austin"
-    //$(".city-search").val();
+    var citySearched = $(".city-search").val();
+
     var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + citySearched + "&units=imperial" + "&appid=" + apiKey
 
     fetch(apiUrl)
@@ -33,7 +33,24 @@ var currentForecast = function(){
         })
         .then(function(data){
             uvIndex = data.value;
-            $("#current-UVIndex").html("<p id='uv-index'> UV Index: " + uvIndex + "</p>")
+            $("#current-UVIndex").html("<p id='uv-index'> UV Index: <span id='uv-color'>" + uvIndex + "</span></p>")
+
+            //color UV index 
+            if (uvIndex >= 11){
+                $("#uv-color").css("background-color", "purple");
+                $("#uv-color").css("color", "White");
+            } else if (uvIndex < 11 && uvIndex > 7){
+                console.log("very high");
+                $("#uv-color").css("background-color", "red");
+                $("#uv-color").css("color", "White");
+            } else if (uvIndex < 8 && uvIndex > 5){
+                $("#uv-color").css("background-color", "orange");
+            } else if (uvIndex < 6 && uvIndex > 2){
+                $("#uv-color").css("background-color", "yellow");
+            } else if (uvIndex <= 2 && uvIndex > 0){
+                $("#uv-color").css("background-color", "green");
+                $("#uv-color").css("color", "White");
+            }
         })
 
         $("#current-city").html("<h1>" + citySearched + "</h1>");
@@ -41,7 +58,7 @@ var currentForecast = function(){
         $("#current-temp").html("<p> Temp: " + currentTemp + "&#8457 </p>")
         $("#current-wind").html("<p> Wind: "+ currentWind +" MPH</p>")
         $("#current-humidity").html("<p> Humidity: " + currentHumidity + "</p>")
-        
+ 
     })
 
 }
